c3d971830eb3b8e927839b85ac08b25052ec56a7
Dawid Brzozowski <d.brzozowski@wp.pl>
2017-12-18 11:45:18 +0100

    Use self

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
+	}
+};
+
+var btn1 = new Button('Hello!');
+btn1.create();
+
+/*
+var btn2 = new Button('tekst');
+btn2.create();
+
+var btn3 = new Button('kliknij mnie!');
+btn3.create();
+*/
